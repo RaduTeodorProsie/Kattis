31c192273073b38e1460e6cae105f9a4d41290e0
MONcalc <psn.radu@gmail.com>
2024-09-22 18:22:19 +0300

    Added the rest of the files.

---
+++ haloween_loot.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<algorithm>
+#include<random>
+#include<chrono>
+#include<queue>
+#include<unordered_map>
+#include<map>
+#include<cmath>
+#include<stack>
+#include<iomanip>
+#include<set>
+#define fi first
+#define se second
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using ld = double;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+constexpr int NMAX = 1e6 + 1;
+constexpr int mod = 1e9 + 7;
+
+map<int,string> dp[101];
+
+void baga(int i, int v, string &s)
+{
+    if(!dp[i].count(v) || s < dp[i][v])
+        dp[i][v] = s;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr),cout.tie(nullptr);
+
+    int n,a[101],b[101]; cin >> n; dp[0][0] = "";
+    for(int i = 1; i <= n ; i++) cin >> a[i];
+    for(int i = 1; i <= n ; i++) cin >> b[i];
+    for(int i = 1; i <= n ; i++)
+        {
+            for(auto &s : dp[i-1])
+                {
+                    int val = s.fi; string ans = s.se; ans += "A";
+                    baga(i,val + a[i],ans); ans.pop_back();
+                    ans += "B"; baga(i,val - b[i],ans);
+                }
+        }
+
+    int vans = 1e9; string sans;
+    for(auto &it : dp[n])
+        {
+            if(abs(it.fi) < vans)
+                {
+                    vans = abs(it.fi);
+                    sans = it.se;
+                }
+            else if(abs(it.fi) == vans)
+                {
+                    if(it.se < sans)
+                        sans = it.se;
+                }
+        }
+
+    cout << sans;
+}
---
+++ xorisland.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+///my favourite <3
+
+constexpr int nmax = 1 << 25;
+
+int dp[nmax], seen[nmax], v[25], n, sz;
+bool sos[nmax];
+
+int main()
+{
+    cin >> n;
+    for(int i = 0 ; i < n ; i++)
+        cin >> v[i];
+    for(int i = 0 ; i < n ; i++)
+        for(int j = i + 1; j < n; j++)
+            for(int k = j + 1; k < n ; k++)
+                if((v[i] ^ v[j] ^ v[k])==0)
+                {   
+                    int ce = (1 << i) | (1 << j) | (1 << k);
+                    sos[ce] = 1;
+                }
+    
+    for(int i = 0; i < n; i++)
+        for(int j = 1; j < (1 << n); j++)
+            if(j & (1 << i)) sos[j] |= sos[j ^ (1 << i)];
+    for(int i = 1; i < (1 << n); i++)
+    {
+        if(!sos[i]) continue;
+        
+        dp[i] = 1e9;
+        for(int b = 0; b < n; b++)
+            if(i & (1 << b))
+                dp[i] = min(dp[i], dp[i ^ (1 << b)] + 1);
+    }
+    
+    cout << dp[(1 << n) - 1];
+}
