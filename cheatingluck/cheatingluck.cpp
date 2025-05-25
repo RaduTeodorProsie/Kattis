6b56b40b06524ce49fbe3ac358cdc130d4f47a3f
Radu-Teodor Prosie <psn.radu@gmail.com>
2025-05-25 21:21:15 +0300

    Update cheatingluck.cpp for the missed case (not present in the tests but still)

--- cheatingluck/cheatingluck.cpp
+++ cheatingluck/cheatingluck.cpp
@@ -3,43 +3,46 @@
 using namespace std;
 
 int main() {
-    int d, g, n, k;
-    cin >> d >> g >> n >> k;
+  int d, g, n, k;
+  cin >> d >> g >> n >> k;
 
-    const int S = d + g;
+  const int S = d + g;
 
-    vector<vector<int>> dp(k + 1, vector<int>(S + 1, 1e9));
-    vector<vector<int>> odp = dp;
+  vector<vector<int>> dp(k + 1, vector<int>(S + 1, 1e9));
+  vector<vector<int>> odp = dp;
 
-    for (int b = 0; b <= S; b++)
-        odp[0][b] = b;
+  for (int b = 0; b <= S; b++)
+    odp[0][b] = b;
 
-    for (int i = n - 1; i >= 0; i--) {
+  for (int i = n - 1; i >= 0; i--) {
 
-        for (int b = 0; b <= S; b++) {
-            dp[0][b] = b;
-        }
-
-        for (int j = 1; j <= k; j++) {
-            for (int b = 0; b <= S; b++) {
-                auto &B = odp[j];
-                auto &A = odp[j - 1];
-                int ans = 0;
-                const int up = min(b, S - b);
-
-                for (int add = 2048; add; add >>= 1) {
-                    if (ans + add > up)
-                        continue;
-                    if (A[b + ans + add] <= B[b - ans - add]) {
-                        ans += add;
-                    }
-                }
-
-                dp[j][b] = A[b + ans];
-            }
-        }
-        swap(dp, odp);
+    for (int b = 0; b <= S; b++) {
+      dp[0][b] = b;
     }
 
-    cout << odp[k][d];
+    for (int j = 1; j <= k; j++) {
+      
+      auto &B = odp[j];
+      auto &A = odp[j - 1];
+      
+      for (int b = 0; b <= S; b++) {
+        int ans = 0;
+        const int up = min(b, S - b);
+
+        for (int add = 1024; add; add >>= 1) {
+          if (ans + add > up)
+            continue;
+          if (A[b + ans + add] <= B[b - ans - add]) {
+            ans += add;
+          }
+        }
+
+        dp[j][b] = A[b + ans];
+        if(ans < up) dp[j][b] = max(dp[j][b], B[b - ans - 1]);
+      }
+    }
+    dp.swap(odp);
+  }
+
+  cout << odp[k][d];
 }
